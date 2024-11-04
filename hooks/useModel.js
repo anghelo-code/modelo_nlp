ee692caa4d21a7d0043acd369feec5ea7f298675
Anghelo Alagon <200518@unsaac.edu.pe>
2024-11-03 23:56:57 -0500

    creat useModel for to use apiModel

---
+++ hooks/useModel.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+export function useModel(inputString) {
+  const [outputMo, setOutput] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const classifyText = async () => {
+      try {
+        setLoading(true);
+        let jsonRes = { "texto": inputString }
+        console.log(`Clasificando el texto: ${jsonRes}`);
+
+        // Hacer la solicitud a la API
+        const response = await fetch('https://apimodelonlp.onrender.com/clasificar', {  // Cambia 'localhost' por tu IP si es necesario
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json' // Asegúrate de incluir el encabezado correcto
+          },
+          body: JSON.stringify({ "texto": inputString }), // Asegúrate de que el cuerpo de la solicitud coincida con lo que espera tu API
+        });
+        if (!response.ok) {
+          throw new Error(`Error en la respuesta: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        // Procesar la respuesta
+        if (data.resultado && data.resultado.length > 0) {
+          setOutput(data.resultado[0] === "SI"); // True si es violento, False si no
+          console.log('data resuelta', data.resultado[0] === "SI");
+        } else {
+          setOutput(null); // O maneja el caso en que no se reciba un resultado válido
+        }
+      } catch (err) {
+        console.error("Error al clasificar el texto:", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (inputString) {
+      classifyText();
+    }
+  }, [inputString]);
+
+  return { outputMo, loading, error };
+}
